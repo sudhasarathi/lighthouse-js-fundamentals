6eb3e59e0bb91b4d271472cc4cf3e4323a74e389
Sudha Parthasarathi <sudhav158@email.com>
2022-11-30 14:42:58 -0700

    ingredi

---
+++ ingredients.js
@@ -0,0 +1,12 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+let i = 0;
+while (i <ingredients.length) {
+  console.log(ingredients[i]);
+  i++;
+};
+for ( i = 0; i < ingredients.length; i++ ){
+  console.log(ingredients[i]);
+};
+for ( i = 7; i < ingredients.length; i++ ){
+  console.log(ingredients.reverse());
+};
