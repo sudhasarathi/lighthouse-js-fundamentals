0932740e03033be3839be6b439d11bc46203c399
Sudha Parthasarathi <sudhav158@email.com>
2022-11-30 14:10:51 -0700

    after while loop

--- packing-list.js
+++ packing-list.js
@@ -4,4 +4,11 @@ console.log("Kitchen stuff to pack:");
 for (let i = 0; i < packingList.length; i++) {
   console.log(packingList[i]);
 }
-packingList[3];
+packingList[3]
+
+console.log("Kitchen stuff to pack:");
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
+}
